2161057320faa8e290bc7f77292f41fa9fe7debf
Uquinhas <lucassrizzo@outlook.com>
2017-06-28 13:19:13 -0300

    Add files via upload

---
+++ NobreFinanceira/portal/pg_btn_menu/pagamento/pagamento.php
@@ -0,0 +1,96 @@
+<?php 
+	session_start(); 
+	include_once('../../../conf/config.php');
+	verificaSessao();
+?>
+<html>
+	<head>
+        <meta charset="UTF-8" />
+        <title>Nobre Financeira - Cadastro de Pagamentos </title>
+        <link href="../../css/reset.css" type="text/css" rel="stylesheet" />
+        <link href="../../css/pagamento.css" rel="stylesheet" type="text/css" />
+	</head>
+	<body>
+        <div id="topo">
+            <div id="container-topo">
+                <div id="container-opcoes">
+                    <ul>
+                        <li><a href="../../../conf/unsets.php">SAIR</a></li>
+                    </ul>
+                </div><!-- Fim container-opcoes-->
+                <div class="" id="logo">
+                </div><!-- Fim Logo -->
+                <div id="menu">
+                    <ul>
+                        <li class="menu-caixa">
+                            <a href="../../index.php">Página Inicial</a>
+                        </li>
+                        <li class="menu-caixa">
+                            <a href="pagamento.php" class="active">Pagamento</a>
+                        </li>
+                        <li class="menu-caixa">
+                            <a href="../recebimento/recebimento.php">Recebimento</a>
+                        </li>
+                        <li class="menu-caixa">
+                            <a href="../lembrar/lembrar.php" id="tremer">Lembre-se!</a>
+                        </li>
+                    </ul>
+                </div> <!-- Fim Menu -->
+            </div><!-- Fim container topo-->
+        </div> <!-- Fim div topo -->
+		<div id="container-informacoes">
+        <div id="botao-container">
+			<a href="tabela_pagamento.php">Ver Pagamentos</a>
+		</div>
+			<h2>Cadastro de Pagamentos</h2>
+			<div id="form-div">  
+
+				<!--**********************************Começa o Formulário***********************************************-->
+				<form method="post" action="action_pagamento.php" id="fContato" > 
+					<fieldset id="formadepagamento">              
+						<legend>
+							<h1> Tipo do Pagamento</h1>
+						</legend>
+						<select name="tFormpag" style="border: solid #999999 1px;">
+							<option>Selecione uma opção</option>
+							<option disabled> </option>
+							<?php 
+								$sql = "SELECT * FROM `tb_tipopagamento`";
+								$opcao = mysqli_query($con, $sql);
+								
+								if(mysqli_num_rows($opcao) > 0){	
+									while($dados = mysqli_fetch_assoc($opcao)){
+										echo '<option value="'.utf8_encode($dados["tipoPagamento"]).'">'.utf8_encode($dados["tipoPagamento"]).'</option>';
+									}
+								}							
+							?>
+						</select>
+							<!--<input type="radio" name="tFormpag" value="Dinheiro" id="cFormpag" checked="checked"/><label for="cFormpag" class="formPagTab" required> Dinheiro </label>
+							<input type="radio" name="tFormpag" value="Cheque" id="cFormadepag" checked="checked"/><label for="cFormadepag" class="formPagTab" required> Cheque</label>
+							<input type="radio" name="tFormpag" value="Cartão de Crédito ou Débito" id="cFormpagamen" checked="checked" required/><label for="cFormpagamen" class="formPagTab"> Cartão de Crédito ou Débito </label>
+							<input type="radio" name="tFormpag" value="Boleto Bancário" id="cFormapaga" checked="checked" required/><label for="cFormapaga" class="formPagTab"> Boleto Bancário</label>-->
+					</fieldset>
+					<fieldset id="DiadoPagamento">   <br />         
+						<p> 
+							<h1>Dia do Pagamento</h1> <br />
+							<input type="date" name="tDia" id="cDia" required/> 
+						</p>
+					</fieldset><br />
+					<fieldset id="valoraPagar">              
+						<p>
+							<label for="cTot"> <h1>Valor do Pagamento</h1> <br /> R$ </label>
+							<input type="text" name="tTot" id="cTot" placeholder="Digite números e pontos" required/>
+						</p><br />
+					</fieldset>
+					<fieldset id="buttonEnviar">              
+						<p> 
+							<input type="submit" value="Enviar" id="btnEnviar"> 
+						</p>
+					</fieldset>
+				</form>
+				<!--**********************************termina o Formulário***********************************************-->
+				
+			</div><!--Form div-->
+		</div><!--Fim container-informações-->
+	</body>
+</html>
