f98310d5dd127a8e577e91705c6e009cc582e466
Uquinhas <lucassrizzo@outlook.com>
2017-06-28 13:18:57 -0300

    Create action_pagamento.php

---
+++ NobreFinanceira/portal/pg_btn_menu/pagamento/action_pagamento.php
@@ -0,0 +1,25 @@
+<?php 
+	session_start(); 
+	include_once('../../../conf/config.php');
+	verificaSessao();
+
+	$tipo = utf8_decode($_POST['tFormpag']);
+	$data = str_replace('-', '', $_POST['tDia']); //Tratamento da variável. Transforma 9999-88-77 em 99998877.
+	$valor = str_replace('.', ',', $_POST['tTot']);
+	
+	$sql = "INSERT INTO `tb_pagamento`(`tipoPagamento`, `dataPagamento`, `valorPagamento`, `pago`) VALUES ('$tipo', '$data', '$valor', 0)";
+	
+	if(mysqli_query($con, $sql)){
+		
+		$local= "\"location.href='/NobreFinanceira/portal/pg_btn_menu/pagamento/pagamento.php'\"";
+		echo '
+			<script> setTimeout(function(){ alert("Cadastrado com sucesso!");}, 0);
+				window.setTimeout('.$local.', 500);
+			</script>
+		';
+	}
+	else
+	{
+		echo "Erro ao cadastrar";
+	}
+?>
